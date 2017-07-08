f1643252fc4f5ebb622b2cee7e7b141659fd6673
Sattar Hummatli <settarxan@gmail.com>
2017-07-08 12:22:02 +0400

    Created first version and configured in jCenter()

---
+++ app/src/main/java/com/hummatli/smartapprater/SampleActivityKotlin.java
@@ -0,0 +1,13 @@
+package com.hummatli.smartapprater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleActivityKotlin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
